ef4f0ab1b7cbbafa123907c8141f94695bd5e20f
emmanGuides <77281902+emmanGuides@users.noreply.github.com>
2021-06-24 17:02:58 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+//search bar
+function searchBar() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+//progress bar
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
